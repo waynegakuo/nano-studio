c06791cfbe7af964617f07a58eab606e14e32f28
Wayne Gakuo <waynejotham@gmail.com>
2025-10-23 08:21:22 +0300

    feat(firebase-setup): Integrate Firebase into the application. Add Firebase configuration, authentication support, and environment-specific settings. Update dependencies to include @angular/fire.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,16 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {environment} from '../environments/environment';
+import {getAuth, provideAuth} from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth())
   ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAzhzjaAr_yQIIEkRM2HfUaj_9hHb83BMo",
+    authDomain: "nano-studios.firebaseapp.com",
+    projectId: "nano-studios",
+    storageBucket: "nano-studios.firebasestorage.app",
+    messagingSenderId: "549434076269",
+    appId: "1:549434076269:web:4ddfc29fec8021f502a364",
+    measurementId: "G-LKTBM9FL8S"
+  }
+};
